750bdb52b607725f982e87886a945cfa070ba3da
jongoh-lee <sky40165285@gmail.com>
2021-12-03 20:07:34 +0900

    redux_prac

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hidd");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import { Provider } from "react-redux";
+import store from "./store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
